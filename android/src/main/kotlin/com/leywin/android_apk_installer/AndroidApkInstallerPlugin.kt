aa097f3b6a06b7f7452c5ae9429127c18ac253bc
leywino <muhammadarshadv2@gmail.com>
2024-10-29 16:34:30 +0530

    add broadcaster

--- android/src/main/kotlin/com/leywin/android_apk_installer/AndroidApkInstallerPlugin.kt
+++ android/src/main/kotlin/com/leywin/android_apk_installer/AndroidApkInstallerPlugin.kt
@@ -1,6 +1,7 @@
 package com.leywin.android_apk_installer
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
@@ -8,10 +9,17 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageInstaller
 import android.net.Uri
+import android.os.Build
 import android.util.Log
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import java.io.File
@@ -19,20 +27,57 @@ import java.io.FileInputStream
 import java.io.IOException
 import java.io.OutputStream
 
-class AndroidApkInstallerPlugin: FlutterPlugin, MethodCallHandler {
+class AndroidApkInstallerPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
   private lateinit var channel: MethodChannel
+  private lateinit var eventChannel: EventChannel
   private lateinit var context: Context
   private lateinit var installResult: Result
+  private var activity: Activity? = null
+  private var events: EventChannel.EventSink? = null
 
   override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "android_apk_installer")
     channel.setMethodCallHandler(this)
+    eventChannel = EventChannel(flutterPluginBinding.binaryMessenger, "app_install_uninstall_events")
+    eventChannel.setStreamHandler(object : EventChannel.StreamHandler {
+      override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+        this@AndroidApkInstallerPlugin.events = events
+        registerInstallUninstallReceiver()
+      }
+
+      override fun onCancel(arguments: Any?) {
+        unregisterInstallUninstallReceiver()
+      }
+    })
     context = flutterPluginBinding.applicationContext
 
     // Register the broadcast receiver for installation completion
     val filter = IntentFilter()
     filter.addAction(PackageInstaller.ACTION_SESSION_COMMITTED)
     context.registerReceiver(installReceiver, filter)
+  }
+
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    context.unregisterReceiver(installReceiver)
+    unregisterInstallUninstallReceiver()
+    channel.setMethodCallHandler(null)
+    eventChannel.setStreamHandler(null)
+  }
+
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    activity = binding.activity
+  }
+
+  override fun onDetachedFromActivity() {
+    activity = null
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    activity = binding.activity
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    activity = null
   }
 
   override fun onMethodCall(call: MethodCall, result: Result) {
@@ -55,7 +100,7 @@ class AndroidApkInstallerPlugin: FlutterPlugin, MethodCallHandler {
         val askPrompt: Boolean? = call.argument("askPrompt")
         if (packageName != null) {
           try {
-            uninstallApk(packageName, askPrompt!! ,result)
+            uninstallApk(packageName, askPrompt!!, result)
           } catch (e: Exception) {
             result.error("UNINSTALLATION_FAILED", "Uninstallation failed: ${e.message}", null)
           }
@@ -63,12 +108,20 @@ class AndroidApkInstallerPlugin: FlutterPlugin, MethodCallHandler {
           result.error("INVALID_ARGUMENT", "Package name is required", null)
         }
       }
+      "setFullScreenMode" -> {
+        val enable: Boolean? = call.argument("enable")
+        if (enable != null) {
+          setFullScreenMode(enable)
+          result.success(null)
+        } else {
+          result.error("INVALID_ARGUMENT", "Enable parameter is required", null)
+        }
+      }
       else -> {
         result.notImplemented()
       }
     }
   }
-
 
   private fun installApk(apkPath: String) {
     val packageInstaller = context.packageManager.packageInstaller
@@ -108,12 +161,14 @@ class AndroidApkInstallerPlugin: FlutterPlugin, MethodCallHandler {
   }
 
   @SuppressLint("MissingPermission")
-  private fun uninstallApk(packageName: String, askPrompt:Boolean,result: Result) {
+  private fun uninstallApk(packageName: String, askPrompt: Boolean, result: Result) {
+    Log.d("Uninstall", "Attempting to uninstall $packageName")
     val packageInstaller = context.packageManager.packageInstaller
 
     try {
-      if(askPrompt){
+      if (askPrompt) {
         uninstallApkWithIntent(packageName)
+        result.success("Prompting for uninstallation")
         return
       }
       packageInstaller.uninstall(packageName, PendingIntent.getBroadcast(
@@ -166,8 +221,67 @@ class AndroidApkInstallerPlugin: FlutterPlugin, MethodCallHandler {
     }
   }
 
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    context.unregisterReceiver(installReceiver)
-    channel.setMethodCallHandler(null)
+  @Suppress("DEPRECATION")
+  private fun setFullScreenMode(enable: Boolean) {
+    activity?.let {
+      if (enable) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+          // For Android 11 (API 30) and above
+          val controller = it.window.insetsController
+          if (controller != null) {
+            controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+            controller.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+          } else {
+            Log.e("setFullScreenMode", "InsetsController is null.")
+          }
+        } else {
+          // For Android versions below 11
+          @Suppress("DEPRECATION")
+          it.window.decorView.systemUiVisibility = (
+                  View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                          or View.SYSTEM_UI_FLAG_FULLSCREEN
+                          or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                          or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                          or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                          or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                  )
+        }
+      } else {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+          // For Android 11 (API 30) and above
+          val controller = it.window.insetsController
+          controller?.show(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+        } else {
+          // For Android versions below 11
+          @Suppress("DEPRECATION")
+          it.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+        }
+      }
+    } ?: Log.e("setFullScreenMode", "Activity is null, unable to set full screen mode.")
+  }
+
+  // Register receiver for app install/uninstall events
+  private fun registerInstallUninstallReceiver() {
+    val filter = IntentFilter().apply {
+      addAction(Intent.ACTION_PACKAGE_ADDED)
+      addAction(Intent.ACTION_PACKAGE_REMOVED)
+      addAction(Intent.ACTION_PACKAGE_REPLACED)
+      addDataScheme("package")
+    }
+    context.registerReceiver(installUninstallReceiver, filter)
+  }
+
+  // Unregister receiver for app install/uninstall events
+  private fun unregisterInstallUninstallReceiver() {
+    context.unregisterReceiver(installUninstallReceiver)
+  }
+
+  // BroadcastReceiver to handle app installation and uninstallation events
+  private val installUninstallReceiver = object : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+      val action = intent?.action
+      val packageName = intent?.data?.encodedSchemeSpecificPart
+      events?.success(mapOf("action" to action, "packageName" to packageName))
+    }
   }
 }
--- lib/android_apk_installer.dart
+++ lib/android_apk_installer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/services.dart';
 
 class AndroidApkInstaller {
   static const MethodChannel _channel = MethodChannel('android_apk_installer');
+  static const eventChannel = EventChannel('app_install_uninstall_events');
 
   /// Install an APK from the specified file path.
   /// Returns a message indicating the result of the installation.
@@ -30,6 +31,29 @@ class AndroidApkInstaller {
       return result ?? "Uninstallation successful";
     } on PlatformException catch (e) {
       return "Failed to uninstall APK: '${e.message}'.";
+    }
+  }
+
+  /// Toggle full-screen mode.
+  /// Pass [enable] as `true` to enable full-screen mode,
+  /// or `false` to disable it and restore the system UI.
+  static Future<void> setFullScreenMode(bool enable) async {
+    try {
+      await _channel.invokeMethod('setFullScreenMode', {'enable': enable});
+    } on PlatformException catch (e) {
+      print("Failed to set full-screen mode: '${e.message}'.");
+    }
+  }
+
+  
+  static Stream<Map<String, dynamic>> get installUninstallEvents async* {
+    await for (var event in eventChannel.receiveBroadcastStream()) {
+      if (event is Map) {
+        yield {
+          "action": event["action"] as String?,
+          "packageName": event["packageName"] as String?
+        };
+      }
     }
   }
 }
